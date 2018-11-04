138d773914ad78e0297f92261a38ae5962f2b87b
Ashley Alofs <aalofs@mistercarwash.com>
2018-11-03 21:53:17 -0700

    finalSaturdaycommit

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -16,14 +16,15 @@ var wins = 0; // How many wins has the player has
 var losses = 0;
 var remainingGuesses= []; //holds number of guesses left
 var guessesLeft = 0; // How many guesses the player has left
-var guessedLetters = []; //holds array of letters player has guessed
-var randomWord = [];
+var guessedLetters = []; //holds array of wrong letters player has guessed
 var wordUnderscores = [];
 var currentWordIndex; // Index of the current word in the array
+var wImg = "shante";
+var lImg = "sashay";
 
 const maxGuesses = 7;
 
-function beginGame (){
+function playGame (){
     guessedLetters = [];
     guessesLeft = 7;
     wins = 0;
@@ -31,6 +32,7 @@ function beginGame (){
     wordUnderscores =[];
 
     randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(randomWord);
    
     for (var i=0; i < randomWord.length; i++){
         wordUnderscores.push("_");
@@ -46,4 +48,43 @@ function beginGame (){
 
 }
 
-beginGame();
+
+document.onkeyup = function(event){
+    letterChoices = event.key;
+
+    if(randomWord.indexOf(letterChoices) > -1){
+        for (var j = 0; j <randomWord.length; j++){
+
+            if (randomWord[j] === letterChoices){
+                wordUnderscores[j]= letterChoices;
+                console.log(wordUnderscores);
+                wins++;
+            }
+        }
+    }
+
+    else{
+        guessedLetters.push(letterChoices);
+        guessesLeft--;
+        losses++;
+        
+        console.log(guessesLeft);
+    };  
+}
+
+function score(){
+    if (letterChoices=== randomWord.length){
+        function winImage(){
+            var wImg = document.getElementById ("shante");
+            wImg.style.visibility= "visible";
+        }
+    }
+    else if(guessesLeft===0){
+        function loseImage(){
+            var lImg = document.getElementById ("sashay");
+            lImg.style.visibility= "visible";
+        }
+    };   
+}
+
+playGame();
